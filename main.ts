87a928de5993a168c6592e6902956075964da17e
dai0115 <hateen0808@gmail.com>
2023-01-29 10:56:41 +0900

    共通化用関数の作成

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 import { App } from "cdktf";
 import { VpcStack } from "./stacks/vpc-stack";
 
+import { devConfig } from "./config/dev";
+
 const app = new App();
-new VpcStack(app, "vpcStack");
+new VpcStack(app, "vpcStack", devConfig);
 app.synth();
--- stacks/vpc-stack.ts
+++ stacks/vpc-stack.ts
@@ -2,7 +2,6 @@ import { Construct } from "constructs";
 import { TerraformStack, Token } from "cdktf";
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
 
-//import { VpcEndpoint } from "../.gen/modules/VpcEndpoint";
 import { Vpc } from "../.gen/modules/Vpc";
 import { Subnets } from "../.gen/modules/Subnets";
 
@@ -10,124 +9,110 @@ import { VpcEndpoint } from "@cdktf/provider-aws/lib/vpc-endpoint";
 import { SecurityGroup } from "@cdktf/provider-aws/lib/security-group";
 import { SecurityGroupRule } from "@cdktf/provider-aws/lib/security-group-rule";
 
+import { ConfigType, SecurityGroupRules, TrafficType } from "../config/types";
+
 export class VpcStack extends TerraformStack {
-  constructor(scope: Construct, id: string) {
+  constructor(scope: Construct, id: string, props: ConfigType) {
     super(scope, id);
 
-    new AwsProvider(this, "AWS", {
-      region: "ap-northeast-1",
+    const { region, prefix } = props;
+    const {
+      vpcCidr,
+      availabilityZones,
+      privateSubnets,
+      privateSubnetNames,
+      intraSubnets,
+      intraSubnetNames,
+      databaseSubnets,
+      databaseSubnetNames,
+    } = props.vpcConfig;
+
+    new AwsProvider(this, `${prefix}-AWS`, {
+      region: region,
     });
 
-    // VPC
-    const vpc = new Vpc(this, "MyVpc", {
-      name: "my-vpc",
-      cidr: "10.0.0.0/16",
+    // VPCの作成
+    const vpc = new Vpc(this, `${prefix}-Vpc`, {
+      name: `${prefix}-Vpc`,
+      cidr: vpcCidr,
       enableDnsHostnames: true,
       enableDnsSupport: true,
-      azs: ["ap-northeast-1a", "ap-northeast-1c"],
-      privateSubnets: ["10.0.0.0/24", "10.0.1.0/24"],
-      privateSubnetNames: ["PrivateAlb1", "PrivateAlb2"],
-      intraSubnets: ["10.0.2.0/24", "10.0.3.0/24"],
-      intraSubnetNames: ["PrivateEcs1", "PrivateEcs2"],
-      databaseSubnets: ["10.0.4.0/24", "10.0.5.0/24"],
-      databaseSubnetNames: ["PrivateDb1", "PrivateDb2"],
+      azs: availabilityZones,
+      privateSubnets: privateSubnets,
+      privateSubnetNames: privateSubnetNames,
+      intraSubnets: intraSubnets,
+      intraSubnetNames: intraSubnetNames,
+      databaseSubnets: databaseSubnets,
+      databaseSubnetNames: databaseSubnetNames,
     });
 
     // VPCendpoint用のサブネットの追加
-    const endpointSubnet = new Subnets(this, "EndpointSubnets", {
+    const endpointSubnet = new Subnets(this, `${prefix}-Subnet`, {
       vpcId: vpc.vpcIdOutput,
       availabilityZones: ["ap-northeast-1a"],
       cidrBlock: "10.0.6.0/24",
       subnetCount: "1",
       tags: {
-        Name: "endpointSubnet",
+        Name: `${prefix}-EndpointSubnet`,
       },
     });
 
-    // ALB用のセキュリティグループの作成
-    const albSG = new SecurityGroup(this, "albSg", {
-      name: "albSG",
-      vpcId: vpc.vpcIdOutput,
-    });
-    // VPCLinkからのinboundのルールは別途必要になった時に作成
-    new SecurityGroupRule(this, "albsg-egress-rule", {
-      cidrBlocks: ["0.0.0.0/0"],
-      type: "egress",
-      fromPort: 0,
-      toPort: 0,
-      protocol: "-1",
-      securityGroupId: albSG.id,
-    });
+    // セキュリティグループの作成
+    const albSG = this.createSecurityGroup("alb", vpc.vpcIdOutput);
+    this.attachAllTrafficRules("alb", "ingress", albSG.id);
+    this.attachAllTrafficRules("alb", "egress", albSG.id);
 
-    // ECS用のセキュリティグループの作成
-    const ecsSG = new SecurityGroup(this, "ecsSG", {
-      name: "ecsSG",
-      vpcId: vpc.vpcIdOutput,
-    });
-    new SecurityGroupRule(this, "ecsSG-ingress-rule", {
-      sourceSecurityGroupId: albSG.id,
-      description: "from ALB",
-      type: "ingress",
+    const ecsSG = this.createSecurityGroup("ecs", vpc.vpcIdOutput);
+    const ecsIngressRules: SecurityGroupRules = {
       fromPort: 8080,
       toPort: 8080,
+      trafficType: "ingress",
       protocol: "tcp",
       securityGroupId: ecsSG.id,
-    });
-    new SecurityGroupRule(this, "ecsSG-egress-rule", {
-      cidrBlocks: ["0.0.0.0/0"],
-      type: "egress",
-      fromPort: 0,
-      toPort: 0,
-      protocol: "-1",
-      securityGroupId: ecsSG.id,
-    });
+    };
+    this.attachSecurityGroupRulesFromSg("alb", albSG.id, ecsIngressRules);
+    this.attachAllTrafficRules("ecs", "egress", ecsSG.id);
 
-    // 踏み台サーバ用のセキュリティグループの作成
-    const bastionSG = new SecurityGroup(this, "bastionSG", {
-      name: "bastionSG",
-      vpcId: vpc.vpcIdOutput,
-    });
-    new SecurityGroupRule(this, "bastionSG-egress-rule", {
-      cidrBlocks: ["0.0.0.0/0"],
-      type: "egress",
+    const bastionSG = this.createSecurityGroup("bastion", vpc.vpcIdOutput);
+    this.attachAllTrafficRules("bastion", "egress", bastionSG.id);
+
+    const endpointSG = this.createSecurityGroup("endpoint", vpc.vpcIdOutput);
+    const endpointIngressRules: SecurityGroupRules = {
+      fromPort: 443,
+      toPort: 443,
+      trafficType: "ingress",
+      protocol: "tcp",
+      securityGroupId: endpointSG.id,
+    };
+    this.attachSecurityGroupRulesCidr(
+      "endpoint",
+      ["10.0.0.0/16"],
+      endpointIngressRules
+    );
+    const endpointIngressRules2: SecurityGroupRules = {
+      fromPort: 443,
+      toPort: 443,
+      trafficType: "ingress",
+      protocol: "tcp",
+      securityGroupId: endpointSG.id,
+    };
+    this.attachSecurityGroupRulesFromSg(
+      "bastion",
+      bastionSG.id,
+      endpointIngressRules2
+    );
+    const endpointEngressRules: SecurityGroupRules = {
       fromPort: 0,
       toPort: 0,
-      protocol: "-1",
+      trafficType: "egress",
+      protocol: "tcp",
       securityGroupId: bastionSG.id,
-    });
-
-    // VPCエンドポイント用のセキュリティグループの作成
-    const endpointSG = new SecurityGroup(this, "endpointSG", {
-      name: "endpointSG",
-      vpcId: vpc.vpcIdOutput,
-    });
-    new SecurityGroupRule(this, "endpointSG-ingress-rule1", {
-      cidrBlocks: ["10.0.0.0/16"],
-      description: "from ALB",
-      type: "ingress",
-      fromPort: 443,
-      toPort: 443,
-      protocol: "tcp",
-      securityGroupId: endpointSG.id,
-    });
-    // こいつはいらないかも
-    new SecurityGroupRule(this, "endpointSG-ingress-rule2", {
-      sourceSecurityGroupId: bastionSG.id,
-      description: "from Bastion",
-      type: "ingress",
-      fromPort: 443,
-      toPort: 443,
-      protocol: "tcp",
-      securityGroupId: endpointSG.id,
-    });
-    new SecurityGroupRule(this, "endpointSG-egress-rule", {
-      cidrBlocks: ["10.0.0.0/16"],
-      type: "egress",
-      fromPort: 0,
-      toPort: 0,
-      protocol: "-1",
-      securityGroupId: endpointSG.id,
-    });
+    };
+    this.attachSecurityGroupRulesCidr(
+      "endpoint",
+      ["10.0.0.0/16"],
+      endpointEngressRules
+    );
 
     // VPCエンドポイントの作成
     new VpcEndpoint(this, "vpc-endpoint-ssm", {
@@ -174,15 +159,72 @@ export class VpcStack extends TerraformStack {
       },
     });
   }
-
-  /*
-  private extractSubnetId(subnets: string[], index: number): string[] {
-    if (subnets === undefined) {
-      throw new Error("subnet is not found");
-    }
-    console.log(subnets);
-    console.log(subnets.slice(index - 1));
-    return subnets.slice(index - 1);
+  /**
+   * @param name - セキュリティグループの名前
+   * @param vpcId - セキュリティグループを作成するvpcのId
+   */
+  private createSecurityGroup(name: string, vpcId: string): SecurityGroup {
+    return new SecurityGroup(this, `${name}SG`, {
+      name: `${name}SG`,
+      vpcId: vpcId,
+      tags: {
+        Name: `${name}SG`,
+      },
+    });
   }
-  */
+
+  private attachAllTrafficRules(
+    name: string,
+    trafficType: TrafficType,
+    securityGroupId: string
+  ) {
+    new SecurityGroupRule(this, `${name}SG-${trafficType}-rule`, {
+      cidrBlocks: ["0.0.0.0/0"],
+      type: trafficType,
+      fromPort: 0,
+      toPort: 0,
+      protocol: "-1",
+      securityGroupId: securityGroupId,
+    });
+  }
+  /**
+   * @param name - セキュリティグループルールの名前
+   * @param cidr - 通信を許可する通信元(先)のCidrBlocks
+   * @param rules - セキュリティグループルールの詳細
+   */
+  private attachSecurityGroupRulesCidr(
+    name: string,
+    cidr: string[],
+    rules: SecurityGroupRules
+  ) {
+    new SecurityGroupRule(this, `${name}SG-${rules.trafficType}-rule`, {
+      cidrBlocks: cidr,
+      type: rules.trafficType,
+      fromPort: rules.fromPort,
+      toPort: rules.toPort,
+      protocol: rules.protocol,
+      securityGroupId: rules.securityGroupId,
+    });
+  }
+  /**
+   * @param name - セキュリティグループルールの名前
+   * @param cidr - 通信を許可する通信元(先)のセキュリティグループのId
+   * @param rules - セキュリティグループルールの詳細
+   *
+   * 通信元がセキュリティグループである場合に利用する
+   */
+  private attachSecurityGroupRulesFromSg(
+    name: string,
+    sourceSecurityGroupId: string,
+    rules: SecurityGroupRules
+  ) {
+    new SecurityGroupRule(this, `${name}SG-${rules.trafficType}-rule`, {
+      sourceSecurityGroupId: sourceSecurityGroupId,
+      type: rules.trafficType,
+      fromPort: rules.fromPort,
+      toPort: rules.toPort,
+      protocol: rules.protocol,
+      securityGroupId: rules.securityGroupId,
+    });
+  }
 }
