19505dee94763d2405817238412bff73e6071014
dai0115 <hateen0808@gmail.com>
2023-02-04 22:06:56 +0900

    ssmエージェントはデフォルトで入っているた不要

--- constructors/bastion_install.sh
+++ constructors/bastion_install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install -y mysql
-sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-sudo systemctl start amazon-ssm-agent
