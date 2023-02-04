c3f6675e9763a43335bd8218e27ab9458e8f9bb9
dai0115 <hateen0808@gmail.com>
2023-02-04 23:01:22 +0900

    configファイルのprops利用とリファクタリング

--- stacks/database-stack.ts
+++ stacks/database-stack.ts
@@ -36,60 +36,129 @@ export class DatabaseStack extends TerraformStack {
 
     const { bastionSG, dbSG, bastionSubnet, subnetIds } = props;
     const { region, prefix } = props.config;
+    const {
+      dbMasterUsername,
+      dbFamily,
+      clusterEngine,
+      clusterEngineVersion,
+      availabilityZones,
+      instanceCount,
+      instanceClass,
+    } = props.config.db;
 
     new AwsProvider(this, `${prefix}-AWS`, {
       region: region,
     });
     new RandomProvider(this, "random", {});
 
-    // Auroraログイン用の踏み台サーバ
+    // Auroraログイン用の踏み台サーバ作成
     new Bastion(this, `${prefix}-Bastion`, {
       config: props.config,
       securityGroup: bastionSG,
       bastionSubnet: bastionSubnet,
     });
 
-    // aurora用のsubnetgroupの作成
-    const subnetGroup = new DbSubnetGroup(this, `${prefix}-subnetgroup`, {
-      name: `${prefix}-subnetgroup`,
-      subnetIds: subnetIds,
-      tags: {
-        Name: `${prefix}-subnetgroup`,
-      },
-    });
+    const kms = this.createKms(prefix);
 
-    // kms作成
-    const kms = new KmsKey(this, `${prefix}-kmskey`, {
-      enableKeyRotation: true,
-      tags: {
-        Name: `${prefix}-kmskey`,
-      },
-    });
-
-    new KmsAlias(this, `${prefix}-kmsAlias`, {
-      name: "alias/kmsalias",
-      targetKeyId: kms.id,
-    });
-
-    // credential保存用のsecretsマネージャ
-    const randomPass = new Password(this, "randomPass", {
+    // SecretsManager作成
+    const randomPass = new Password(this, `${prefix}-randomPass`, {
       length: 16,
       special: true,
       overrideSpecial: "!#$%&*()-_=+[]{}<>:?",
     });
+    this.createSecret(prefix, dbMasterUsername, randomPass.result);
 
-    const secret = new SecretsmanagerSecret(this, "secret", {
-      name: "taurusroot1", // こいつはシークレット自体の名前
+    const subnetGroup = this.createSubnetGroup(prefix, subnetIds);
+
+    const parameter = this.createParameterGroup(prefix, dbFamily);
+
+    // rdsクラスターの作成
+    const cluster = new RdsCluster(this, "rds-cluster", {
+      clusterIdentifier: `${prefix}-auroraCluster`,
+      engine: clusterEngine,
+      engineVersion: clusterEngineVersion,
+      availabilityZones: availabilityZones,
+      masterUsername: dbMasterUsername,
+      masterPassword: randomPass.result,
+      dbClusterParameterGroupName: parameter.name,
+      kmsKeyId: kms.arn,
+      vpcSecurityGroupIds: [dbSG.id],
+      //deletionProtection: true TODO:ある程度まとまったらコメントアウトを解除
+      skipFinalSnapshot: true,
+      storageEncrypted: true,
+      dbSubnetGroupName: subnetGroup.name,
+      enabledCloudwatchLogsExports: ["audit", "error", "general", "slowquery"],
+      preferredBackupWindow: "16:00-16:30",
     });
 
-    new SecretsmanagerSecretVersion(this, "secret-version", {
+    // rdsクラスターインスタンスの作成
+    new RdsClusterInstance(this, `${prefix}-auroraClusterInstance`, {
+      count: instanceCount,
+      identifier: `${prefix}-auroraClusterInstance`,
+      clusterIdentifier: cluster.id,
+      instanceClass: instanceClass,
+      engine: cluster.engine,
+      engineVersion: cluster.engineVersion,
+    });
+  }
+
+  /**
+   * @param name - kmsの名前
+   * auroraの暗号化に利用するkmsを作成する
+   */
+  private createKms(name: string): KmsKey {
+    const kms = new KmsKey(this, `${name}-kmskey`, {
+      enableKeyRotation: true,
+      tags: {
+        Name: `${name}-kmskey`,
+      },
+    });
+    new KmsAlias(this, `${name}-kmsAlias`, {
+      name: `alias/${name}`,
+      targetKeyId: kms.id,
+    });
+    return kms;
+  }
+  /**
+   * @param name - Secretsの名前
+   * @param username - SecretsManagerに保存するdbのユーザ名
+   * @param pass - SecretsManagerに保存するdbのパスワード
+   * SecretsManagerに格納するシークレットを作成
+   */
+  private createSecret(name: string, username: string, pass: string) {
+    const secret = new SecretsmanagerSecret(this, `${name}-secret`, {
+      name: `${name}-secret`,
+    });
+    new SecretsmanagerSecretVersion(this, `${name}-secretVersion`, {
       secretId: secret.id,
       secretString: JSON.stringify({
-        password: randomPass.result,
+        username: username,
+        password: pass,
       }),
     });
-
-    // パラメータグループ
+  }
+  /**
+   * @param name - Secretsの名前
+   * @param subnetIds - auroraを配置するサブネットのidsを受け取る
+   * auroraを配置するサブネットグループを作成
+   */
+  private createSubnetGroup(name: string, subnetIds: string[]): DbSubnetGroup {
+    return new DbSubnetGroup(this, `${name}-subnetGroup`, {
+      name: `${name}-subnetGroup`,
+      subnetIds: subnetIds,
+      tags: {
+        Name: `${name}-subnetGroup`,
+      },
+    });
+  }
+  /**
+   * @param name - Secretsの名前
+   * SecretsManagerに格納するシークレットを作成
+   */
+  private createParameterGroup(
+    name: string,
+    dbFamily: string
+  ): RdsClusterParameterGroup {
     const parameterSettings: RdsClusterParameterGroupParameter[] = [
       {
         name: "character_set_server",
@@ -136,39 +205,10 @@ export class DatabaseStack extends TerraformStack {
         value: "1",
       },
     ];
-    const parameter = new RdsClusterParameterGroup(this, "parameterGroup", {
-      name: "rds-cluster-for-parameter-group",
-      family: "aurora-mysql8.0",
+    return new RdsClusterParameterGroup(this, `${name}-parameterGroup`, {
+      name: `${name}-parameterGroup`,
+      family: dbFamily,
       parameter: parameterSettings,
-    });
-
-    // rdsクラスター
-    const cluster = new RdsCluster(this, "rds-cluster", {
-      clusterIdentifier: "aurora-cluster",
-      engine: "aurora-mysql",
-      engineVersion: "8.0.mysql_aurora.3.02.2",
-      availabilityZones: ["ap-northeast-1a"],
-      masterUsername: "taurusroot2", // configで定義したものを受け取る
-      masterPassword: randomPass.result,
-      dbClusterParameterGroupName: parameter.name,
-      kmsKeyId: kms.arn,
-      vpcSecurityGroupIds: [dbSG.id],
-      //deletionProtection: true TODO:ある程度まとまったらコメントアウトを解除
-      skipFinalSnapshot: true,
-      storageEncrypted: true,
-      dbSubnetGroupName: subnetGroup.name,
-      enabledCloudwatchLogsExports: ["audit", "error", "general", "slowquery"],
-      preferredBackupWindow: "16:00-16:30",
-    });
-
-    // rdsクラスターインスタンス
-    new RdsClusterInstance(this, "rds-cluster-instance", {
-      count: 1,
-      identifier: "rds-cluster-instance",
-      clusterIdentifier: cluster.id,
-      instanceClass: "db.t4g.large",
-      engine: cluster.engine,
-      engineVersion: cluster.engineVersion,
     });
   }
 }
