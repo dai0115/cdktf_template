00c0be86a81215c27cb0def709bd42a582eda9f2
dai0115 <hateen0808@gmail.com>
2023-03-29 16:49:00 +0900

    changed directory name

--- util/bastion/create_task.sh
+++ util/bastion/create_task.sh
@@ -4,7 +4,8 @@
 ENV=dev
 CLUSTER=application-$ENV-BastionCluster
 SUBNETS=$(aws ec2 describe-subnets --filter "Name=tag:Name,Values=application-dev-Bastion" | jq -r '.Subnets[].SubnetId')
-SECURITY_GROUPS=$(aws ec2 describe-security-groups --filters "Name=tag:Name,Values=bastionSG" | jq -r '.SecurityGroups[].GroupId')
+# SECURITY_GROUPS=$(aws ec2 describe-security-groups --filters "Name=tag:Name,Values=bastionSG" | jq -r '.SecurityGroups[].GroupId')
+SECURITY_GROUPS=sg-0d30c9d73c692c610
 
 # start task
 aws ecs run-task --cluster $CLUSTER --count 1 --launch-type FARGATE \
